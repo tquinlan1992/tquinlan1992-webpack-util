31dafc60a930f56b391a3891fa857eef55dd19ce
Thomas Quinlan <tquinlan1992@gmail.com>
2017-10-15 11:58:27 -0400

    update index.ts

---
+++ index.ts
@@ -0,0 +1,9 @@
+import awesomeTypescriptLoader from './awesome-typescript-loader';
+import sassLoader from './sassLoader';
+import sourceMapLoader from './source-map-loader';
+
+export default {
+    awesomeTypescriptLoader,
+    sassLoader,
+    sourceMapLoader
+}
