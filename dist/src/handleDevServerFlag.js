4a9e7135f44b15df5ba944fe8e2ae16745e743ac
Tom <tquinlan1992@gmail.com>
2017-10-27 22:37:40 -0400

    use 0.0.0.0

--- dist/src/handleDevServerFlag.js
+++ dist/src/handleDevServerFlag.js
@@ -4,7 +4,7 @@ var WebpackDevServer = require('webpack-dev-server');
 function handleDevServerFlag(compiler) {
     var server = new WebpackDevServer(compiler, {});
     var port = 8080;
-    server.listen(port, "localhost", function () {
+    server.listen(port, "0.0.0.0", function () {
         console.log("webpack dev server listening on port: " + port);
     });
 }
--- src/handleDevServerFlag.ts
+++ src/handleDevServerFlag.ts
@@ -3,7 +3,7 @@ const WebpackDevServer = require('webpack-dev-server');
 function handleDevServerFlag(compiler: any) {
     const server = new WebpackDevServer(compiler, {});
     const port = 8080;
-    server.listen(port, "localhost", () => {
+    server.listen(port, "0.0.0.0", () => {
         console.log("webpack dev server listening on port: " + port)
     });
 }
