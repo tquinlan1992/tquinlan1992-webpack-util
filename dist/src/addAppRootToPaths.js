10a571491a0e497bd54e96dde9f243dac4a59409
Thomas Quinlan <tquinlan1992@gmail.com>
2017-10-27 23:49:59 -0400

    don't use app path for public directory

--- dist/src/addAppRootToPaths.js
+++ dist/src/addAppRootToPaths.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var appRoot = require('app-root-path');
 function addAppRootToPaths(config) {
+    var entry = config.entry, appOutputPath = config.appOutputPath, tsconfig = config.tsconfig;
     var absolutePathsConfig = Object.assign(config, {
-        entry: appRoot + config.entry,
-        appOutputPath: appRoot + config.appOutputPath,
-        tsconfig: appRoot + config.tsconfig
+        entry: appRoot + entry,
+        appOutputPath: appRoot + appOutputPath,
+        tsconfig: appRoot + tsconfig
     });
     return absolutePathsConfig;
 }
--- src/addAppRootToPaths.ts
+++ src/addAppRootToPaths.ts
@@ -1,12 +1,11 @@
 const appRoot = require('app-root-path');
 
 function addAppRootToPaths(config: any) {
-    const { entry, appOutputPath, tsconfig, publicDirContext } = config;
+    const { entry, appOutputPath, tsconfig } = config;
     const absolutePathsConfig = (<any>Object).assign(config, {
         entry: appRoot + entry,
         appOutputPath: appRoot + appOutputPath,
-        tsconfig: appRoot + tsconfig,
-        publicDirContext: appRoot + publicDirContext
+        tsconfig: appRoot + tsconfig
     });
 
     return absolutePathsConfig;
