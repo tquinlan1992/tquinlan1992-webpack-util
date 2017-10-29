aa4f604e7726b56e1ef480eec89876f3fdede1c9
Thomas Quinlan <tquinlan1992@gmail.com>
2017-10-29 18:18:28 -0400

    update path to jest.config.js

--- dist/src/jest/index.js
+++ dist/src/jest/index.js
@@ -4,5 +4,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var shell = require("shelljs");
 var executeJest = "node ./node_modules/jest/bin/jest.js ";
-var configFlagWithPath = "--config ./node_modules/tquinlan1992-webpack-util/jest/jestConfig.js";
+var configFlagWithPath = "--config ./node_modules/tquinlan1992-webpack-util/src/jest/jest.config.js";
 shell.exec(executeJest + configFlagWithPath);
--- src/jest/index.ts
+++ src/jest/index.ts
@@ -3,6 +3,6 @@ import * as shell from 'shelljs';
 
 const executeJest = "node ./node_modules/jest/bin/jest.js "
 
-const configFlagWithPath ="--config ./node_modules/tquinlan1992-webpack-util/jest/jestConfig.js";
+const configFlagWithPath ="--config ./node_modules/tquinlan1992-webpack-util/src/jest/jest.config.js";
 
 shell.exec(executeJest + configFlagWithPath);
