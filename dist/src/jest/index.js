df8b39750fc0c7e4656effef5adb265753a97a2e
Thomas Quinlan <tquinlan1992@gmail.com>
2017-10-29 23:19:03 -0400

    user jest commands

--- dist/src/jest/index.js
+++ dist/src/jest/index.js
@@ -3,28 +3,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var shell = require("shelljs");
-var commandLineArgs = require("command-line-args");
-function addJestFlags(params) {
-    var updateSnapshot = params.updateSnapshot, coverage = params.coverage;
-    var jestOptions = "";
-    if (updateSnapshot) {
-        jestOptions += "--updateSnapshot ";
-    }
-    if (coverage) {
-        jestOptions += "--coverage ";
-    }
-    return jestOptions;
+function getUserJestArgs() {
+    var args = process.argv;
+    return args.slice(2).join(" ");
 }
-var optionDefintions = [
-    {
-        name: 'updateSnapshot', alias: 'v', type: Boolean, defaultValue: false,
-    },
-    {
-        name: 'coverage', type: Boolean, defaultValue: false
-    }
-];
-var options = commandLineArgs(optionDefintions);
 var executeJest = "node ./node_modules/jest/bin/jest.js ";
 var configFlagWithPath = "--config ./node_modules/tquinlan1992-webpack-util/src/jest/jest.config.js ";
-var jestFlags = addJestFlags(options);
-shell.exec(executeJest + configFlagWithPath + jestFlags);
+var userJestArgs = getUserJestArgs();
+shell.exec(executeJest + configFlagWithPath + userJestArgs);
--- src/jest/index.ts
+++ src/jest/index.ts
@@ -1,37 +1,15 @@
 import * as shell from 'shelljs';
-import * as commandLineArgs from 'command-line-args';
 
-function addJestFlags(params: { updateSnapshot: Boolean, coverage: Boolean }): String {
-    const { updateSnapshot, coverage } = params;
-
-    let jestOptions = "";
-    if (updateSnapshot) {
-        jestOptions += "--updateSnapshot "
-    }
-
-    if (coverage) {
-        jestOptions += "--coverage "
-    }
-
-    return jestOptions;
+function getUserJestArgs(): String {
+    const args = process.argv
+    return args.slice(2).join(" ")
 }
-
-const optionDefintions = [
-    {
-        name: 'updateSnapshot', alias: 'v', type: Boolean, defaultValue: false,
-    },
-    {
-        name: 'coverage', type: Boolean, defaultValue: false
-    }
-]
-
-const options = commandLineArgs(optionDefintions);
-
 
 const executeJest = "node ./node_modules/jest/bin/jest.js "
 
 const configFlagWithPath = "--config ./node_modules/tquinlan1992-webpack-util/src/jest/jest.config.js ";
 
-const jestFlags = addJestFlags(options);
 
-shell.exec(executeJest + configFlagWithPath + jestFlags);
+const userJestArgs = getUserJestArgs();
+
+shell.exec(executeJest + configFlagWithPath + userJestArgs);
