5e41c07aefc53f4987767a8dfa35696007357f41
Thomas Quinlan <tquinlan1992@gmail.com>
2017-10-29 23:50:33 -0400

    use app path for jest

--- dist/src/jest/index.js
+++ dist/src/jest/index.js
@@ -3,11 +3,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var shell = require("shelljs");
+var appRoot = require('app-root-path');
 function getUserJestArgs() {
     var args = process.argv;
     return args.slice(2).join(" ");
 }
-var executeJest = "node ./node_modules/jest/bin/jest.js ";
+var jestPath = appRoot + "/node_modules/jest/bin/jest.js";
+var executeJest = "node " + jestPath + " ";
 var configFlagWithPath = "--config ./node_modules/tquinlan1992-webpack-util/src/jest/jest.config.js ";
 var userJestArgs = getUserJestArgs();
 shell.exec(executeJest + configFlagWithPath + userJestArgs);
--- src/jest/index.ts
+++ src/jest/index.ts
@@ -1,11 +1,14 @@
 import * as shell from 'shelljs';
+const appRoot = require('app-root-path');
 
 function getUserJestArgs(): String {
     const args = process.argv
     return args.slice(2).join(" ")
 }
 
-const executeJest = "node ./node_modules/jest/bin/jest.js "
+const jestPath = appRoot + "/node_modules/jest/bin/jest.js"
+
+const executeJest = "node " + jestPath + " ";
 
 const configFlagWithPath = "--config ./node_modules/tquinlan1992-webpack-util/src/jest/jest.config.js ";
 
